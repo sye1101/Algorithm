fdbb79876cd07e27c1117474fdb1f8ce50a2549d
sye1101 <shinye1101@naver.com>
2025-04-19 23:34:37 +0900

    [feat]: ch07 이진 탐색 문제 풀이

---
+++ 5. 이진 탐색/부품 찾기(이진 탐색).py
@@ -0,0 +1,39 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+# N(가게의 부품 개수) 입력
+n = int(input())
+# 가게에 있는 전체 부춤 번호를 공백으로 구분하여 입력
+array = list(map(int, input().split()))
+array.sort() # 이진 탐색을 수행하기 위해 사전에 정렬 수행
+# M(손님이 확인 요청한 부품 개수) 입력
+m = int(input())
+# 손님이 확인 요청한 부품 번호를 공백으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    # 해당 부품이 존재하는지 확인
+    result = binary_search(array, i, 0, n-1)
+    if result != None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+
+"""
+5
+8 3 7 9 2
+3
+5 7 9
+
+출력: no yes yes 
+"""
---
+++ 5. 이진 탐색/부품 찾기(집합 자료형).py
@@ -0,0 +1,13 @@
+# N(가게의 부품 개수)을 입력받기
+n = int(input())
+# 가게에 있는 전체 부품 번호를 입력받아서 집합(set) 자료형에 기록
+array = set(map(int, input().split()))
+
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if i in array:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
