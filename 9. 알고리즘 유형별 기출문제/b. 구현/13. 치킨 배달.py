95dcc39e8235d24e8ba51146215eb39d8cbd6e13
sye1101 <shinye1101@naver.com>
2025-05-06 22:39:45 +0900

    [feat]: ch09 구현 문제 풀이

--- 9. 알고리즘 유형별 기출문제/b. 구현/13. 치킨 배달.py
+++ 9. 알고리즘 유형별 기출문제/b. 구현/13. 치킨 배달.py
@@ -1,24 +1,30 @@
 from itertools import combinations
-
+# N: 도시의 가로, 세로 크기
+# M: 치킨집의 최대 개수
+# 0: 빈칸, 1: 집, 2: 치킨집
 n, m = map(int, input().split())
 chicken, house = [], []
 
 for r in range(n):
     data = list(map(int, input().split()))
     for c in range(n):
-        if data[c] == 1: # 집
+        if data[c] == 1:
             house.append((r, c))
-        elif data[c] == 2: # 치킨집
+        elif data[c] == 2:
             chicken.append((r, c))
 
-candidates = list(combinations(chicken, m)) # 치킨집 조합 찾기
+# 모든 치킨집 중에서 m개의 치킨집을 뽑는 조합 계산
+candidates = list(combinations(chicken, m))
 
+# 치킨 거리의 합을 계산하는 함수
 def get_sum(candidate):
     result = 0
     for hx, hy in house:
+        # 가장 가까운 치킨집 찾기
         tmp = 1e9
         for cx, cy in candidate:
             tmp = min(tmp, abs(hx - cx) + abs(hy - cy))
+        # 가장 가까운 치킨집까지의 거리를 더하기
         result += tmp
     return result
 
@@ -27,3 +33,36 @@ for candidate in candidates:
     result = min(result, get_sum(candidate))
 
 print(result)
+
+"""
+5 3
+0 0 1 0 0
+0 0 2 0 1
+0 1 2 0 0
+0 0 1 0 0
+0 0 0 0 2
+
+출력: 5
+"""
+
+"""
+5 2
+0 2 0 1 0
+1 0 1 0 0
+0 0 0 0 0
+2 0 0 1 1
+2 2 0 1 2
+
+출력: 10
+"""
+
+"""
+5 1
+1 2 0 0 0
+1 2 0 0 0
+1 2 0 0 0
+1 2 0 0 0
+1 2 0 0 0
+
+출력: 11
+"""
